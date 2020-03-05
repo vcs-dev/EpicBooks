f9127c10235b76859303124f06fdffa804a8eb2a
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2020-03-05 00:32:02 -0300

    Estrutura inicial do projeto.

---
+++ Web/Areas/Gerencial/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Gerencial.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        [Area("Gerencial")]
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+    }
+}
