a21f771ff4db4d7b119383c6899db85148fe8b84
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2020-03-12 23:41:33 -0300

    Correção no front.

--- Web/wwwroot/js/produto/produto.js
+++ Web/wwwroot/js/produto/produto.js
@@ -1,10 +1,36 @@
 $(document).ready(function () {
+    const valStatus = $('#status').val();
     $('#status').on('change', function () {
-        alert('asodao');
-        $('#motivoMudancaStatus').removeClass('d-none');
-        if ($('#status').val() === "1")
-            $('#categoriaAtivacao').removeClass('d-none');
-        else if ($('#status').val() === "0")
-            $('#categoriaInativacao').removeClass('d-none');
+        if ($('#status').val() !== valStatus) {
+            $('#divMudaStatus').removeClass('d-none');
+            $('#motivoMudancaStatus').prop('required', 'true');
+        }
+        else {
+            $('#divMudaStatus').addClass('d-none');
+            $('#motivoMudancaStatus').val('');
+            $('#motivoMudancaStatus').removeAttr('required');
+        }
+        if ($('#status').val() === "1" && $('#status').val() !== valStatus) {
+            $('#divCatAtivacao').removeClass('d-none');
+            $('#categoriaAtivacao').prop('required', 'true');
+            $('#divCatInativacao').addClass('d-none');
+            $('#categoriaInativacao').val('');
+            $('#categoriaInativacao').removeAttr('required');
+        }
+        else if ($('#status').val() === "0" && $('#status').val() !== valStatus) {
+            $('#divCatInativacao').removeAttr('required');
+            $('#categoriaInativacao').prop('required', 'true');
+            $('#divCatAtivacao').addClass('d-none');
+            $('#categoriaAtivacao').removeAttr('required');
+            $('#categoriaAtivacao').val('');
+        }
+        else {
+            $('#divCatAtivacao').addClass('d-none');
+            $('#categoriaAtivacao').val('');
+            $('#categoriaAtivacao').removeAttr('required');
+            $('#divCatInativacao').addClass('d-none');
+            $('#categoriaInativacao').val('');
+            $('#categoriaInativacao').removeAttr('required');
+        }
     });
 });
