40d30e3260bf64d63e42ef196898ce3c3b4dc58e
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2020-06-01 20:16:19 -0300

    Pequeno ajuste no index.js

--- Web/wwwroot/js/loja/minhaconta/index.js
+++ Web/wwwroot/js/loja/minhaconta/index.js
@@ -4,10 +4,11 @@
 
     $('.mensagem').on('click', function () {
         var indice = $('.mensagem').index(this);
+        
         $('.modal-title').text($('.tituloNotf').eq(indice).text());
         $('#textoModal').text($('.inputDescricao').eq(indice).val() + ' Data: ' + $('.dataNotf').eq(indice).text());
-        $('#modalMensagem').modal('show');
         $('#btnOkModal').val($('.inputId').eq(indice).val());
+        $('#modalMensagem').modal('show');
     });
 
     $('#btnOkModal').on('click', function () {
