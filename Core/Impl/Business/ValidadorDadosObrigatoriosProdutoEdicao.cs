bdc03ca7fc6f72fe3894d6ad9afacf9a4f43c1e7
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2020-06-29 17:14:13 -0300

    Ajustes no front-end de produto

--- Core/Impl/Business/ValidadorDadosObrigatoriosProdutoEdicao.cs
+++ Core/Impl/Business/ValidadorDadosObrigatoriosProdutoEdicao.cs
@@ -1,4 +1,6 @@
-﻿using Core.Interfaces;
+﻿using Core.Application;
+using Core.Impl.Control;
+using Core.Interfaces;
 using Domain;
 using Domain.Produto;
 using System;
@@ -15,6 +17,8 @@ namespace Core.Impl.Business
             if (entidade.GetType().Name.Equals("Livro"))
             {
                 Livro livro = (Livro)entidade;
+                List<Livro> livros;
+                Result resultado;
 
                 if (string.IsNullOrEmpty(livro.Nome) || string.IsNullOrWhiteSpace(livro.Nome) ||
                     string.IsNullOrEmpty(livro.Descricao) || string.IsNullOrWhiteSpace(livro.Descricao) ||
@@ -26,12 +30,33 @@ namespace Core.Impl.Business
                     string.IsNullOrEmpty(livro.Isbn) || string.IsNullOrWhiteSpace(livro.Isbn) ||
                     livro.QtdePaginas == null || livro.QtdePaginas == 0 || livro.TipoCapa == 0)
                     return "Os campos com * são de preenchimento obrigatório.";
-                //if (livro.Status == 0 && (string.IsNullOrEmpty(livro.MotivoMudancaStatus) || string.IsNullOrWhiteSpace(livro.MotivoMudancaStatus) ||
-                //    livro.CategoriaInativacao == null || livro.CategoriaInativacao == 0))
-                //    return "Os campos com * são de preenchimento obrigatório.";
-                //else if (livro.Status == 1 && (string.IsNullOrEmpty(livro.MotivoMudancaStatus) || string.IsNullOrWhiteSpace(livro.MotivoMudancaStatus) ||
-                //    livro.CategoriaAtivacao == null || livro.CategoriaAtivacao == 0))
-                //    return "Os campos com * são de preenchimento obrigatório.";
+
+                resultado = new Facade().Consultar(new Livro { Id = livro.Id });
+                livros = new List<Livro>();
+                if (resultado.Msg != null)
+                    return "Erro ao validar edição do livro";
+                else
+                {
+                    foreach (var item in resultado.Entidades)
+                    {
+                        livros.Add((Livro)item);
+                    }
+                }
+                if (livros.FirstOrDefault().Status != livro.Status)
+                {
+                    if(string.IsNullOrEmpty(livro.MotivoMudancaStatus) || string.IsNullOrWhiteSpace(livro.MotivoMudancaStatus))
+                        return "Os campos com * são de preenchimento obrigatório.";
+                    if (livro.Status == 1)
+                    {
+                        if(livro.CategoriaAtivacao < 1)
+                            return "Os campos com * são de preenchimento obrigatório.";
+                    }
+                    else if (livro.Status == 0)
+                    {
+                        if (livro.CategoriaInativacao < 1)
+                            return "Os campos com * são de preenchimento obrigatório.";
+                    }
+                }
             }
             else
             {
