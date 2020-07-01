9389e43431834cacd69e8c5ef44ffe4eace9d1f2
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2020-07-01 18:31:22 -0300

    Correção de RN de alteração de endereço

--- Core/Impl/Business/ValidadorDadosObrigatoriosEndereco.cs
+++ Core/Impl/Business/ValidadorDadosObrigatoriosEndereco.cs
@@ -14,6 +14,7 @@ namespace Core.Impl.Business
                 if(endereco.TipoEndereco < 1 || endereco.TipoLogradouro < 1 || endereco.TipoResidencia < 1 ||
                     endereco.Cidade < 1 || endereco.Estado < 1 || endereco.Pais < 1 || endereco.Numero == null || endereco.Numero < 1 ||
                     string.IsNullOrEmpty(endereco.Logradouro) || string.IsNullOrWhiteSpace(endereco.Logradouro) ||
+                    string.IsNullOrEmpty(endereco.Cep) || string.IsNullOrWhiteSpace(endereco.Cep) ||
                     string.IsNullOrEmpty(endereco.Bairro) || string.IsNullOrWhiteSpace(endereco.Bairro))
                 {
                     return "Os campos com * são de preenchimento obrigatório";
